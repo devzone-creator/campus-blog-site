e384e8e32e90c4a0bc64af87b5864aada60609fd
devzone-creator <munkailasule3@gmail.com>
2025-06-10 09:12:48 +0000

    update

--- public/script.js
+++ public/script.js
@@ -2,3 +2,8 @@ window.onclick = function(event) {
     const modal = document.getElementById('blogModal');
     if (event.target == modal) modal.style.display = "none";
 };
+
+document.getElementById('closeModal').addEventListener('click', function(event) {
+    const modal = document.getElementById('blogModal');
+    modal.style.display = "none";
+})
